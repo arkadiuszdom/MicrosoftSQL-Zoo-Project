d7c7f95c27675ee1e606e35d0a60a456fdd2fdd9
Łukasz Zysiak <zysiak.lukasz@gmail.com>
2019-06-03 17:43:12 +0200

    Moduł badania

--- projekt_gosia.sql
+++ projekt_gosia.sql
@@ -1,5 +1,18 @@
 use Zoo
 
+select * from grafik
+select * from ilosc_zmian
+select * from magazyn;
+
+CREATE TRIGGER wyzwalacz
+    ON Zoo.dbo.ilosc_zmian
+ AFTER update
+AS
+BEGIN
+	PRINT 'Wykonano zmiany, dodano wpis do grafika i zaktualizowano ilosc zmian'
+END
+GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -24,22 +37,29 @@ begin
 		intersect 
 		select pracownik_id from ilosc_zmian where ilosc = @sprawdzenie) as tmp
 	declare @pracownik as int = (select top 1(pracownik_id) from tmpFerdeen)
-	if @pracownik = 0
-	print 'Nie ma pracownika ktory moze wykonac to zadanie';
-	else 
-	insert into Zoo..grafik values(@pracownik, (select obowiazek_id from obowiazek where identyfikator = @identyfikator), @data, @zmiana)
-	declare @pomocnicza as int = (select ilosc from ilosc_zmian where pracownik_id = @pracownik) + 1;
-	update ilosc_zmian set ilosc = @pomocnicza where pracownik_id = @pracownik;
+	begin try
+		if @pracownik = 0
+		Print 'Nie ma pracownika ktory moze wykonac to zadanie';
+		else 
+		insert into Zoo..grafik values(@pracownik, (select obowiazek_id from obowiazek where identyfikator = @identyfikator), @data, @zmiana)
+		declare @pomocnicza as int = (select ilosc from ilosc_zmian where pracownik_id = @pracownik) + 1;
+		update ilosc_zmian set ilosc = @pomocnicza where pracownik_id = @pracownik;
+	end try
+	begin catch
+		Print Error_Message()
+	end catch
 	drop table tmpFerdeen;
 end
 go
+
+exec wybor_magazyniera 'Magazyn', 'p', 'lr', '04-04-2019';
+
 
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 create procedure kontrola_magazynu
---procedura zlecajaca uzupelnienie magazynu konkretnej osobie, wyliczenie ilosci, dodanie rekordu 'ostatnie uzupelnienie'
 	@kategoria as varchar(15), @id_produktu as int
 as 
 begin
@@ -50,24 +70,50 @@ begin
 end 
 go 
 
-exec kontrola_magazynu 'Magazyn', 3; 
-
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 create procedure karmienie
---procedura wyliczajaca ilosc karmy na klatke (potem z uwzglednieniem planow zywieniowych), sprawdzenie czy sie zgadza stan magazynu
---wydanie polecenia uzupelnienia magazynu 
-	@klatka as int, @id_produktu as int 
+	@klatka as int
 as 
 begin 
-	declare @ile_zwierzat as int = (select count(zwierze_id) from zwierzeta where klatka_id = @klatka);
-	declare @ilosc_magazyn as int = 50 * @ile_zwierzat * 3;
-	declare @pomocnicza as int = (select ilosc from magazyn where produkt_id = @id_produktu);
-	update magazyn set ilosc = @pomocnicza - @ilosc_magazyn where produkt_id = @id_produktu;
-	if(select ilosc from magazyn where produkt_id = @id_produktu) <= (select ilosc_minimalna from magazyn where produkt_id = @id_produktu)
-		exec kontrola_magazynu 'Magazyn', @id_produktu;
+	create table zwierzePomocnicza (pr_k int NOT NULL PRIMARY KEY IDENTITY(1,1),
+		z_id int NOT NULL)
+	insert into zwierzePomocnicza(z_id) select zwierze_id from zwierzeta where klatka_id = @klatka
+
+	select * from zwierzePomocnicza
+	declare @indeks as int = 1;
+	while (@indeks <= (select max(pr_k) from zwierzePomocnicza))
+		begin
+	
+		create table tabelaPomocnicza
+		( 
+		pk int NOT NULL PRIMARY KEY IDENTITY(1,1),
+		p_id int NOT NULL,
+		ilo int not null);
+
+		declare @zmienna as int = (select z_id from zwierzePomocnicza where pr_k = @indeks);
+		insert into tabelaPomocnicza(p_id, ilo) select * from Zoo..dobierz_produkty_dla_zwierzecia(@zmienna);
+
+		declare @pomocnicza as int = 1;
+		while (@pomocnicza <= (select max(pk) from tabelaPomocnicza))
+		begin 
+			declare @ilosc_magazyn as int = (select ilosc from magazyn where produkt_id = (select p_id from tabelaPomocnicza where pk = @pomocnicza))
+			declare @roznica as int = @ilosc_magazyn - (select ilo from tabelaPomocnicza where pk = @pomocnicza)
+			update magazyn set ilosc = @roznica where produkt_id = (select p_id from tabelaPomocnicza where pk = @pomocnicza)
+			if((select ilosc from magazyn where produkt_id = (select p_id from tabelaPomocnicza where pk = @pomocnicza)) <= (select ilosc_minimalna from magazyn where produkt_id = (select p_id from tabelaPomocnicza where pk = @pomocnicza)))
+			begin
+			declare @id_pomocnicze as int = (select p_id from tabelaPomocnicza where pk = @pomocnicza)
+			exec kontrola_magazynu 'Magazyn', @id_pomocnicze;
+			end 
+			set @pomocnicza = @pomocnicza + 1;
+			end 
+		select * from tabelaPomocnicza
+		drop table tabelaPomocnicza
+		set @indeks = @indeks + 1;
+		end
+		drop table zwierzePomocnicza
 end 
 go 
 
@@ -89,6 +135,7 @@ begin
 	select pracownik_id from pracownicy_klatki where klatka_id = @klatka
 	intersect 
 	select pracownik_id from ilosc_zmian where ilosc = @sprawdzenie) as i) ; 
+	Print @spr
 	if @spr = 0
 		set @sprawdzenie = @sprawdzenie + 1
 	end
@@ -106,12 +153,37 @@ begin
 	declare @pomocnicza as int = (select ilosc from ilosc_zmian where pracownik_id = @pracownik) + 1;
 	update ilosc_zmian set ilosc = @pomocnicza where pracownik_id = @pracownik;
 	drop table tmpFerdeen;
-	if @stanowisko = 'Karmienie' or @stanowisko = 'Leki' 
-		exec karmienie @klatka, 1;
+	if @stanowisko = 'Karmienie'
+		exec karmienie @klatka;
 end
 go
 
-exec wybor_pracownika 'Karmienie', 'w', 2, 'kr', '02-04-2019';
+exec wybor_pracownika 'Karmienie', 'p', 9, 'kr', '04-04-2019';
 
-select * from zwierzeta
+select * from grafik
+select * from ilosc_zmian
 select * from magazyn
+
+select * from pracownicy
+select * from kierownicy
+select * from pracownicy_klatki;
+
+create function kierownik_dzialu
+(@zmienna as varchar(50)) returns varchar(50)
+as
+begin
+declare @kierownik as varchar(50)
+set @kierownik = (select imie + ' ' + nazwisko from kierownicy where sektor = @zmienna)
+
+	return @kierownik;
+
+end 
+go
+
+select Zoo.dbo.kierownik_dzialu('Karmienie') 
+
+update magazyn set ilosc = 230 where produkt_id = 2
+
+select * from magazyn
+
+select * from grafik
--- projekt_tabele.sql
+++ projekt_tabele.sql
@@ -1,3 +1,4 @@
+
 if exists(select 1 from master.dbo.sysdatabases where name = 'Zoo') drop database Zoo
 GO
 CREATE DATABASE Zoo
@@ -153,16 +154,16 @@ CREATE TABLE Zoo..lekarze (
 	lekarz_id int NOT NULL ,
 	imie varchar(30) NOT NULL,
 	nazwisko varchar(30) NOT NULL,
+	ilosc_zmian int,
     PRIMARY KEY (lekarz_id)
 );
 GO
 
 CREATE TABLE Zoo..lekarz_zwierze (
-	lekarz_id int NOT NULL ,
-	zwierze_id int NOT NULL ,
-	lekarz_zwierze_id int NOT NULL ,
-	PRIMARY KEY (lekarz_zwierze_id),
-	foreign key (lekarz_id) references lekarze(lekarz_id) ,
+	lekarz_id int,
+	zwierze_id int,
+	lekarz_zwierze_id int  primary key IDENTITY(1,1),
+	foreign key (lekarz_id) references lekarze(lekarz_id),
 	foreign key (zwierze_id) references zwierzeta(zwierze_id)
 );
 GO
@@ -205,7 +206,8 @@ GO
 CREATE TABLE Zoo..indywidualne_plany_zywieniowe (
     plan_zywieniowy_id int NOT NULL,
     zwierze_id int NOT NULL,
-    PRIMARY KEY (plan_zywieniowy_id, zwierze_id),
+	data_poczatek date, 
+	data_koniec date,
 	foreign key(zwierze_id) references zwierzeta(zwierze_id),
 	foreign key(plan_zywieniowy_id) references plany_zywieniowe(plan_zywieniowy_id)
 );
@@ -225,15 +227,6 @@ CREATE TABLE Zoo..plany_zywieniowe_wartosci_odzywcze (
 	foreign key(wartosc_odzywcza_id) references wartosci_odzywcze(wartosc_odzywcza_id)
 );
 GO
---Arek 20.05.19
-CREATE TABLE Zoo..wartosci_rozmyte_atrybutu_dojrzalosc_plciowa(
-	gatunek_id int NOT NULL,
-	odleglosc_do_lewego_kranca int NOT NULL,
-	odleglosc_do_prawego_kranca int NOT NULL,
-	PRIMARY KEY(gatunek_id, odleglosc_do_lewego_kranca, odleglosc_do_prawego_kranca),
-	foreign key(gatunek_id) references gatunki(gatunek_id)
-);
-GO
 --edit Arek 12.05.19 usunieto tabele produkty, ujednoliconi=o ja z magazyn, przepisano kluczobcy prod-wart.odz.
 CREATE TABLE Zoo..produkty_wartosci_odzywcze (
     produkt_id int NOT NULL,
@@ -245,21 +238,14 @@ CREATE TABLE Zoo..produkty_wartosci_odzywcze (
 GO
 
 
-
 --Robert&Rafał
-GO
-CREATE TABLE Zoo..Rezerwacja(
-	#id_rezerwacji  int NOT NULL,
-	Ilosc_osob int NOT NULL,
-	PRIMARY KEY (#id_rezerwacji)
-	);
 
 GO
 CREATE TABLE Zoo..Promocja(
 	#id_promocji int NOT NULL,
 	procent_obnizki int NOT NULL,
 	nazwa varchar NOT NULL,
-	PRIMARY KEY (#id_promocji)
+	PRIMARY KEY (#id_promocji),
 
 	);
 GO
@@ -267,24 +253,20 @@ CREATE TABLE Zoo..Bilety(
 	#id_biletu int NOT NULL,
 	pawilon_id int NOT NULL,
 	termin date NOT NULL,
-	id_rezerwacji int,
 	obnizka int,
-	cena int NOT NULL,
+	cena int NOT NULL
 	PRIMARY KEY (#id_biletu),
 	foreign key (pawilon_id) references Zoo..pawilony(pawilon_id),
 	foreign key (obnizka) references Zoo..Promocja(#id_promocji),
-	FOREIGN KEY (id_rezerwacji) references Zoo..Rezerwacja(#id_rezerwacji)
 	 );
 GO
-CREATE TABLE Zoo..PawilonyBilety(
+CREATE TABLE Zoo..Rezerwacja(
+	#id_rezerwacji  int NOT NULL,
 	#id_biletu int NOT NULL,
-	pawilon_id int NOT NULL,
-	Liczba_pawilonow int NOT NULL,
-	foreign key (#id_biletu) references Zoo..Bilety(#id_biletu),
-	foreign key (pawilon_id) references Zoo..pawilony(pawilon_id)
+	ilosc_osob int NOT NULL,
+	PRIMARY KEY (#id_rezerwacji),
+	FOREIGN KEY (#id_biletu) references Zoo..Bilety(#id_biletu),
 	);
-	
-
 GO
 CREATE TABLE Zoo..Opinie(
 	#id_opini int NOT NULL,
@@ -294,7 +276,7 @@ CREATE TABLE Zoo..Opinie(
 	komentarz varchar(255),
 	PRIMARY KEY (#id_opini),
 	FOREIGN KEY (#id_biletu) references Zoo..Bilety(#id_biletu),
-	FOREIGN KEY (pawilon_id) references Zoo..pawilony(pawilon_id)
+	FOREIGN KEY (pawilon_id) references Zoo..pawilony(pawilon_id),
 	);
 GO
 CREATE TABLE Zoo..Zaplata(
@@ -305,3 +287,5 @@ CREATE TABLE Zoo..Zaplata(
 	PRIMARY KEY (#id_transakcji),
 	FOREIGN KEY (id_biletow) references Zoo..Bilety(#id_biletu)
 	);
+
+
